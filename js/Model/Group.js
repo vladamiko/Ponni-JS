3f751bd1a484e8fb6ef5d2ab2a16d5d0db14e3fe
Alex <Alex@MacBook-Pro-admin.local>
2017-07-19 16:27:36 +0300

    Change classes

--- js/Model/Group.js
+++ js/Model/Group.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Group {
     constructor () {
         this.name = ''; // value from input
--- js/Model/Tests.js
+++ js/Model/Tests.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Test {
     constructor () {
         this.name = '';
