dfb51d6cc1c7baf599e016da21acfba8b52e9d89
Alex <korzhlesha@gmail.com>
2017-07-23 17:55:35 +0300

    change filtersTpl

--- project/js/Settings/View/tpl/filterTpl.js
+++ project/js/Settings/View/tpl/filterTpl.js
@@ -1,11 +1,15 @@
 'use strict';
 
-function filterTpl () {
+function filterTpl (filterList) {
+    let filters = '';
+
+    filterList.forEach((filter) => {
+        filters += `<div class="filter">${filter}</div>`;
+    });
+
     return `<div class="filter-list-wrapper">
                 <h3>Filter</h3>
-                <div class="filter-list">    
-                    <div class="filter"></div>
-                </div>
+                <div class="filter-list">${filters}</div>
                 <button class="add-filter-btn btn">add filter</button>
             </div>`;
 }
