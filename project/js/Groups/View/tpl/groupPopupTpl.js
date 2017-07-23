629f380c9ba058150fd5a04e4f4ad55b8d34fc9a
vladamiko <vladamiko@gmail.com>
2017-07-23 19:31:52 +0300

    strategy test

--- project/js/Groups/View/tpl/groupPopupTpl.js
+++ project/js/Groups/View/tpl/groupPopupTpl.js
@@ -8,8 +8,8 @@ function groupPopupTpl (directionList) {
     });
 
     return `<div id="group-popup" class="modal-content">
-                <input type="text" id="modalGroupName" class="modal-group-name" placeholder="Enter group name">
-                <select id="modalGroupDirection" class="modal-group-direction">${options}</select>
+                <input type="text" id="modal-group-name" class="modal-group-name" placeholder="Enter group name">
+                <select id="modal-group-direction" class="modal-group-direction">${options}</select>
                 <button id="close-group-btn" class="btn btn-cog">
                     <i class="fa fa-check" aria-hidden="true"></i>
                 </button>
--- project/js/Settings/Model/DirectionModel.js
+++ project/js/Settings/Model/DirectionModel.js
@@ -7,6 +7,68 @@ class DirectionModel {
         this.filterList = [];
     }
 
+    getDirectionTest (test) {
+        let obj = {
+            UI: () => {
+                this.testList = ['Eng1', 'Eng2', 'Eng3', 'Eng4', 'Core', 'Essay'];
+            },
+            GO: () => {
+                this.testList = ['Eng1', 'Eng2', 'Eng3', 'Eng4', 'Core', 'Essay'];
+            },
+            PHP: () => {
+                this.testList = ['Eng1', 'Eng2', 'Eng3', 'Eng4', 'Core', 'Essay'];
+            }
+        }
+
+        return obj[test]();
+    }
+
+    getDirectionFilter (direction) {
+        let obj = {
+            UI: () => {
+                this.filterList = [{
+                    name: 'English 1',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }, {
+                    name: 'English 2',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }, {
+                    name: 'English 3',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }, {
+                    name: 'English 4',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }, {
+                    name: 'Core',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }, {
+                    name: 'Essay',
+                    action: action;
+                    condition: condition;
+                    grade: 60%;
+                }];
+            },
+            GO: () => {
+                this.filterList = [];
+            },
+            PHP: () => {
+                this.filterList = [];
+            }
+        }
+
+        return obj[direction]();
+    }
+
     addTest (testName) {
         this.testList.push(new Test(testName));
     }
