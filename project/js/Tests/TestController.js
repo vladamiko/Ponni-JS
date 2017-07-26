c1913df0e92aadd3a4a1a192c80ffacaa130b53d
vladamiko <vladamiko@gmail.com>
2017-07-26 10:41:01 +0300

    tests

--- project/js/Tests/TestController.js
+++ project/js/Tests/TestController.js
@@ -1,7 +1,25 @@
 'use strict';
 
-class TestController {
+let TestListView = require('../Tests/View/TestListView.js');
 
+class TestController {
+    constructor () {
+        this.subscribe();
+    }
+
+    subscribe () {
+        mediator.sub('group:active', () => {
+            let testListView = new TestListView();
+
+            testListView.appendTest();
+        });
+    }
+
+    showTestList () {
+        let testListView = new TestListView();
+
+        testListView.render();
+    }
 }
 
 module.exports = TestController;
