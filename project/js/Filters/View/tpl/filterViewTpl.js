eb7a94438545c15c121adfd5b61ee3f379643235
vladamiko <vladamiko@gmail.com>
2017-07-26 18:02:28 +0300

    filterViewTpl

--- project/js/Filters/View/tpl/filterViewTpl.js
+++ project/js/Filters/View/tpl/filterViewTpl.js
@@ -5,12 +5,16 @@ function filterViewTpl (filter) {
     let filterList = filter.filterList,
         filterListName = '';
 
-    filterList.forEach((filter) => filterListName += `<div class="filter">
-                                                        <div>${filter.testList}</div>
-                                                        <div>${filter.action} </div>
-                                                        <div>${filter.condition} </div>
-                                                        <div>${filter.grade}</div>
-                                                    </div>`);
+    filterList.forEach((filter) => filterListName += `<div class="filter-item">
+            <input type="checkbox" class="filter-checkbox">
+                <div class="filter">
+                    <div>${filter.testList}</div>
+                    <div>${filter.action} </div>
+                    <div>${filter.condition} </div>
+                    <div>${filter.grade}</div>
+                </div>
+            <button class="btn filter-btn">123</button>
+        </div>`);
 
     return filterListName;
 }
